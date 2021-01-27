4e04c254d628b4313c7b776f8abde9f3b8eb542e
ZabeMath <35405030+ZabeMath@users.noreply.github.com>
2021-01-27 21:12:25 +0100

    Update universities.sh

--- wikidata/universities.sh
+++ wikidata/universities.sh
@@ -1,2 +1,29 @@
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Goethe-Universität Frankfurt am Main" Q50662
 python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Albert-Ludwigs-Universität Freiburg" Q153987
 python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Greifswald" Q165528
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Justus-Liebig-Universität Gießen" Q317053
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Georg-August-Universität Göttingen" Q152838
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Friedrich-Alexander-Universität Erlangen-Nürnberg" Q40025
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Helmut-Schmidt-Universität" Q530991
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Hamburg" Q156725
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Ruprecht-Karls-Universität Heidelberg" Q151510
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Helmstedt" Q314466
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Ingolstadt" Q462039
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Friedrich-Schiller-Universität Jena" Q154561
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Christian-Albrechts-Universität zu Kiel" Q156737
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität zu Köln" Q54096
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Albertus-Universität Königsberg" Q672420
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Konstanz" Q835440
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Leipzig" Q154804
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Johannes Gutenberg-Universität Mainz" Q161982
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Mannheim" Q317070
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Philipps-Universität Marburg" Q155354
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Martin-Luther-Universität Halle-Wittenberg" Q32120
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität der Bundeswehr München" Q283099
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Technischen Universität München" Q157808
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Ukrainischen Freien Universität" Q646136
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Westfälischen Wilhelms-Universität Münster" Q168426
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Ludwig-Maximilians-Universität München" Q55044
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Passau" Q704468
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Regensburg" Q574571
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Rostock" Q159895
