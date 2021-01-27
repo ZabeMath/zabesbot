c28803f1b4c35a58d17c9cc791cf0b225937cd7b
ZabeMath <35405030+ZabeMath@users.noreply.github.com>
2021-01-27 20:57:48 +0100

    Create universities.sh

---
+++ wikidata/universities.sh
@@ -0,0 +1,2 @@
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Albert-Ludwigs-Universität Freiburg" Q153987
+python3 claim_P69_from_dewiki.py "Kategorie:Absolvent der Universität Greifswald" Q165528
