c84c9990e792218a311b6aa3dc96344db42a0a59
ZabeMath <alec@vc-celle.de>
2021-01-29 00:27:16 +0100

    Update claim_citizenship_from_dewiki.py

--- wikidata/citizenship/claim_citizenship_from_dewiki.py
+++ wikidata/citizenship/claim_citizenship_from_dewiki.py
@@ -9,17 +9,26 @@ cat = pywikibot.Category(site,category)
 gen = pagegenerators.CategorizedPageGenerator(cat)
 for page in gen:
     item = pywikibot.ItemPage.fromPage(page)
+    print(item)
+    b_create = False
     if item.claims:
         if 'P31' in item.claims:
             for subclaim in item.claims['P31']:
-                if subclaim == pywikibot.ItemPage(repo, "Q5"):
-                    claim = pywikibot.Claim(repo, u'P27')
-                    claim.setTarget(country)
-                    importedfrom = pywikibot.Claim(repo, u'P143')
-                    dewiki = pywikibot.ItemPage(repo, "Q48183")
-                    importedfrom.setTarget(dewiki)
-                    heuristic = pywikibot.Claim(repo, u'P887')
-                    from_cat = pywikibot.ItemPage(repo, u'Q87206960')
-                    heuristic.setTarget(from_cat)
-                    claim.addSources([importedfrom, heuristic])
-                    item.addClaim(claim)
+                if subclaim.getTarget() == pywikibot.ItemPage(repo, "Q5"):
+                    b_create = True
+        if 'P27' in item.claims:
+            for subclaim in item.claims['P27']:
+                if subclaim.getTarget() == country:
+                    b_create = False
+    if b_create:
+        print("create claim")
+        claim = pywikibot.Claim(repo, u'P27')
+        claim.setTarget(country)
+        importedfrom = pywikibot.Claim(repo, u'P143')
+        dewiki = pywikibot.ItemPage(repo, "Q48183")
+        importedfrom.setTarget(dewiki)
+        heuristic = pywikibot.Claim(repo, u'P887')
+        from_cat = pywikibot.ItemPage(repo, u'Q87206960')
+        heuristic.setTarget(from_cat)
+        claim.addSources([importedfrom, heuristic])
+        item.addClaim(claim)
