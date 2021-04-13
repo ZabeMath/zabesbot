789760efdfd3af8a650c230d2de9ec663c9c52ec
ZabeMath <35405030+ZabeMath@users.noreply.github.com>
2021-04-13 23:16:15 +0200

    Add Cisleithanien and Kaisertum Österreich

--- wikidata/citizenship/citizenship.sh
+++ wikidata/citizenship/citizenship.sh
@@ -1,3 +1,5 @@
 python3 claim_citizenship_from_dewiki.py Kategorie:Österreicher Q40
 python3 claim_citizenship_from_dewiki.py Kategorie:Schweizer Q39
 python3 claim_citizenship_from_dewiki.py Kategorie:Liechtensteiner Q347
+python3 claim_citizenship_from_dewiki.py "Kategorie:Person (Cisleithanien)" Q533534
+python3 claim_citizenship_from_dewiki.py "Kategorie:Person (Kaisertum Österreich)" Q131964
