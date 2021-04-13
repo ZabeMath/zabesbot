094c8d7b786cab98133ceb9a1f8f315f24f9d4b1
ZabeMath <35405030+ZabeMath@users.noreply.github.com>
2021-04-13 23:27:59 +0200

    Add check if page exists

--- wikidata/citizenship/claim_citizenship_from_dewiki.py
+++ wikidata/citizenship/claim_citizenship_from_dewiki.py
@@ -17,23 +17,24 @@ country = pywikibot.ItemPage( repo, sys.argv[2] )
 cat = pywikibot.Category( site, category )
 gen = pagegenerators.CategorizedPageGenerator( cat )
 for page in gen:
-    item = pywikibot.ItemPage.fromPage( page )
-    print( item )
-    b_create = True
-    if item.claims:
-        if 'P27' in item.claims:
-            for subclaim in item.claims['P27']:
-                if subclaim.getTarget() == country:
-                    b_create = False
-    if b_create and is_human( repo, item ):
-        print( "create claim" )
-        claim = pywikibot.Claim( repo, u'P27' )
-        claim.setTarget( country )
-        importedfrom = pywikibot.Claim( repo, u'P143' )
-        dewiki = pywikibot.ItemPage( repo, "Q48183" )
-        importedfrom.setTarget( dewiki )
-        heuristic = pywikibot.Claim( repo, u'P887' )
-        from_cat = pywikibot.ItemPage( repo, u'Q87206960' )
-        heuristic.setTarget( from_cat )
-        claim.addSources( [importedfrom, heuristic] )
-        item.addClaim( claim )
+    if page:
+        item = pywikibot.ItemPage.fromPage( page )
+        print( item )
+        b_create = True
+        if item.claims:
+            if 'P27' in item.claims:
+                for subclaim in item.claims['P27']:
+                    if subclaim.getTarget() == country:
+                        b_create = False
+        if b_create and is_human( repo, item ):
+            print( "create claim" )
+            claim = pywikibot.Claim( repo, u'P27' )
+            claim.setTarget( country )
+            importedfrom = pywikibot.Claim( repo, u'P143' )
+            dewiki = pywikibot.ItemPage( repo, "Q48183" )
+            importedfrom.setTarget( dewiki )
+            heuristic = pywikibot.Claim( repo, u'P887' )
+            from_cat = pywikibot.ItemPage( repo, u'Q87206960' )
+            heuristic.setTarget( from_cat )
+            claim.addSources( [importedfrom, heuristic] )
+            item.addClaim( claim )
